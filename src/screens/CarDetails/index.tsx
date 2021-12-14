e8eaa53dc2b719a810db00f57bf69989915885cd
Bruno3c <brunodossantos.job@gmail.com>
2021-12-14 18:56:56 -0300

    componente de slider

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
 import {
   Container,
-  Header
+  Header,
+  CarImages
 } from './styles';
 
 export function CarDetails(){
-  return (
+
+  const imageUrl = [
+    'https://w7.pngwing.com/pngs/38/708/png-transparent-car-mercedes-car-love-compact-car-vehicle-thumbnail.png'
+  ];
+
+    return (
     <Container>
       <Header>
         <BackButton
           onPress={() => {}}
         />
       </Header>
+      <CarImages>
+        <ImageSlider 
+          imagesUrl={[imageUrl[0]]}
+        />
+      </CarImages>
     </Container>
   );
 }
