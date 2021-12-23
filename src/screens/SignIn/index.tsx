4c61c4f975c8e4bdf53c482e43f35d2174c97acd
Bruno3c <brunodossantos.job@gmail.com>
2021-12-23 10:50:46 -0300

    accessibilidade em formularios

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Keyboard, KeyboardAvoidingView } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { PasswordInput } from '../../components/PasswordInput';
@@ -15,39 +17,43 @@ import {
 
 export function SignIn(){
   return (
-    <Container>
-      <Header>
-        <Title>Estamos{`\n`}quase lá.</Title>
-        <SubTitle>Faça seu login para começar{`\n`}uma esperiência incrível.</SubTitle>
-      </Header>
-      <Form>
-        <Input 
-          iconName="mail"
-          placeholder="E-mail"
-          keyboardType="email-address"
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
-        <PasswordInput 
-          iconName="lock"
-          placeholder="Senha"
-        />
-      </Form>
-      <Footer>
-        <Button
-          title="Login"
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-        />
-        <Button
-          title="Cadastrar gratuitamente"
-          color={theme.colors.background_secondary}
-          onPress={() => {}}
-          light
-          loading={false}
-        />
-      </Footer>
-    </Container>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <Title>Estamos{`\n`}quase lá.</Title>
+            <SubTitle>Faça seu login para começar{`\n`}uma esperiência incrível.</SubTitle>
+          </Header>
+          <Form>
+            <Input 
+              iconName="mail"
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCorrect={false}
+              autoCapitalize="none"
+            />
+            <PasswordInput 
+              iconName="lock"
+              placeholder="Senha"
+            />
+          </Form>
+          <Footer>
+            <Button
+              title="Login"
+              onPress={() => {}}
+              enabled={false}
+              loading={false}
+            />
+            <Button
+              title="Cadastrar gratuitamente"
+              color={theme.colors.background_secondary}
+              onPress={() => {}}
+              light
+              loading={false}
+            />
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
